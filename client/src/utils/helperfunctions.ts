a523b71b66eef3c0067e942003745308392cc22c
RalfiSlask <nilssonnarki@gmail.com>
2024-03-04 17:29:38 +0100

    refactor: change spelling

--- client/src/utils/helperfunctions.ts
+++ client/src/utils/helperfunctions.ts
@@ -2,14 +2,14 @@
  * Swaps class between two elements
  * First element has the class added, second has it removed
  * @param {Element | null} firstElement
- * @param {Element | null} SecondElement
+ * @param {Element | null} secondElement
  * @param {string} className
  */
 export function swapClassBetweenTwoElements(
   firstElement: Element | null,
-  SecondElement: Element | null,
+  secondElement: Element | null,
   className: string
 ) {
   firstElement?.classList.add(className);
-  SecondElement?.classList.remove(className);
+  secondElement?.classList.remove(className);
 }
