dd236a5bbf679135a9566909abc4eecd4a9475a6
RalfiSlask <nilssonnarki@gmail.com>
2024-03-06 08:16:03 +0100

    add ids to button in function appendUserToList, handle click on user buttons with event delegation and emitting their status to server, catching io.emits for user status and playersready and update how many players are ready and their status text, also put all socket on functions in DOMContentLoaded

--- client/src/main.ts
+++ client/src/main.ts
@@ -9,6 +9,7 @@ const loginButton = document.getElementById('loginButton');
 const loginSection = document.getElementById('loginSection');
 const gameLobbySection = document.getElementById('gameLobbySection');
 const gameLobbyList = document.getElementById('gameLobbySectionUl');
+const playersReadyContainer = document.getElementById('playersReady');
 
 /**
  * Handles login for user
@@ -46,6 +47,7 @@ loginButton?.addEventListener('click', () => {
 /**
  * Adds a user to the lobby list with a ready button. Each user's name is displayed
  * with a specific color defined by the user's `color` property.
+ * sets button id to user id
  * @param {object} user - The user object that contains username, ID and color.
  * @param {string} user.username - The users name.
  * * @param {string} user.color - The CSS color code (as a string) for the user's name.
@@ -58,7 +60,8 @@ function appendUserToList(user: { username: string; color: string; id: string })
   userElement.innerText = user.username;
   userElement.style.color = user.color;
   const readyButton = document.createElement('button');
-  readyButton.innerText = 'Ready';
+  readyButton.id = user.id;
+  readyButton.innerText = 'waiting';
   userElement.appendChild(readyButton);
   const listItem = document.createElement('li');
   listItem.appendChild(userElement);
@@ -71,7 +74,7 @@ function appendUserToList(user: { username: string; color: string; id: string })
  * Each user is displayed as a list item with their name in the specified color and their socket ID.
  */
 
-export function initializeUserList(): void {
+function initializeUserList(): void {
   socket.on('updateUserList', (users: Array<{ username: string; color: string; id: string }>) => {
     if (!gameLobbyList) return;
     gameLobbyList.innerHTML = '';
@@ -79,4 +82,53 @@ export function initializeUserList(): void {
   });
 }
 
-initializeUserList();
+/**
+ * With event delegation checks if target is button
+ * If target is button, change player status and send to server together with button id
+ * @param {Even} e - click event
+ * @returns void
+ */
+function handleClickOnButtons(e: Event) {
+  const target = e.target as HTMLElement;
+  if (target.tagName !== 'BUTTON') return;
+  const currentStatus = target.textContent === 'waiting' ? 'ready' : 'waiting';
+  socket.emit('userStatus', { statusText: currentStatus, statusId: target.id });
+}
+
+/**
+ * Socket on from which status the user has, catches io.emit
+ * Changes the button text to the status
+ */
+function recieveSocketUserStatus() {
+  socket.on('userStatus', status => {
+    if (!gameLobbyList) return;
+    const buttons = gameLobbyList.querySelectorAll('button');
+    const { statusId, statusText } = status;
+    buttons.forEach(button => {
+      if (button.id !== statusId) return;
+      button.textContent = statusText;
+    });
+  });
+}
+
+/**
+ * Updates how many players are currently ready
+ */
+function recieveSocketPlayersReady() {
+  socket.on('playersReady', players => {
+    if (!playersReadyContainer) return;
+    playersReadyContainer.textContent = `${players}/5`;
+  });
+}
+
+function initialFunctionsOnLoad() {
+  initializeUserList();
+  recieveSocketUserStatus();
+  recieveSocketPlayersReady();
+}
+
+document.addEventListener('DOMContentLoaded', initialFunctionsOnLoad);
+
+gameLobbyList?.addEventListener('click', e => {
+  handleClickOnButtons(e);
+});
