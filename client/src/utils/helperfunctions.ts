4fe67cea1ab70081410d433be9e7a23f3e45db96
RalfiSlask <nilssonnarki@gmail.com>
2024-03-11 07:19:22 +0100

    feat: another update to make sure only sharper colors are generated

--- client/src/utils/helperfunctions.ts
+++ client/src/utils/helperfunctions.ts
@@ -20,9 +20,17 @@ export function swapClassBetweenTwoElements(
  * @returns random color
  */
 export function getRandomColor() {
-  const minNumberRGB = 0x00000;
-  const maxNumberRGB = 0xcccccc;
-  const range = maxNumberRGB - minNumberRGB;
-  const getRandomColor = `#${(Math.floor(Math.random() * range) + minNumberRGB).toString(16)}`;
-  return getRandomColor;
+  const maximumR = 0xcc;
+  const maximumG = 0xcc;
+  const maximumB = 0xcc;
+
+  const r = Math.floor(Math.random() * maximumR);
+  const g = Math.floor(Math.random() * maximumG);
+  const b = Math.floor(Math.random() * maximumB);
+
+  const hexR = r.toString(16).padStart(2, '0');
+  const hexG = g.toString(16).padStart(2, '0');
+  const hexB = b.toString(16).padStart(2, '0');
+
+  return `#${hexR}${hexG}${hexB}`;
 }
