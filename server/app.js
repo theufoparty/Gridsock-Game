79fbd2238817c8ec49a5dca04f9ccf565e92d1f3
Nell <eemandersson@outlook.com>
2024-03-12 18:58:41 +0100

    refactor get random word logic

--- server/app.js
+++ server/app.js
@@ -30,14 +30,12 @@ app.get('/', (req, res) => {
 let gameArray = [];
 
 function getRandomWord() {
-  
   const randomWordId = Math.floor(Math.random() * gameArray.length);
   let currentWord = gameArray[randomWordId];
   //console.log(gameArray.length + ' ' + randomWordId + ' ' + currentWord.id + currentWord.word);
   gameArray.splice(randomWordId, 1); //Splice from array
   //console.log('gameArray', gameArray);
-  let randomWord = currentWord.word; 
-  io.emit('words', randomWord);
+  return currentWord.word;
 }
 
 /**
@@ -52,13 +50,12 @@ app.get('/words', (req, res) => {
     .toArray()
     .then(data => {
       res.json(data);
-      const wordArray = data[0].words;  
+      const wordArray = data[0].words;
       if (gameArray.length === 0) {
         gameArray = wordArray;
-        getRandomWord();
-      } else {
-        getRandomWord();
-      } 
+      }
+      const randomWord = getRandomWord();
+      io.emit('words', randomWord);
     });
 });
 
